a68dd4674e95a64e2ca28338c916e91903287016
zk8pr4pbcy-sudo <zk8pr4pbcy@privaterelay.appleid.com>
2026-08-10 05:20:02 +0300

    Temporary inspect uploaded image blobs

--- assets/js/home-mobile-qa.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  "use strict";
-
-  var dictionaries = window.AVENTURA_I18N || (window.AVENTURA_I18N = {});
-  dictionaries.ar = Object.assign({}, dictionaries.ar || {}, {
-    "home.heroTitle": "تبدأ حكاية أفنتورا من جدة التاريخية.",
-    "home.heroDescription": "نصمم تجارب خاصة وفعاليات وضيافة للزوار وضيوف الشركات والتنفيذيين، تبدأ من جدة التاريخية وتمتد إلى البحر الأحمر والصحراء والطائف ضمن خطة واحدة منسقة بعناية."
-  });
-}());
